c97bcbff8da5bffb3ba524bca552075ace2a7bba
Daniel Campón Perdigones <campondaniel@gmail.com>
2025-05-03 14:04:57 +0000

    feat: configuración inicial del canvas - Integrado canvas en el componente GameCanvas con tamaño 800x600 - Conectado useRef para acceder al contexto 2D - Añadido useEffect para preparar entorno de dibujo - Dibujada cuadrícula de referencia basada en celdas de 20x20px

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -1,15 +1,51 @@
-import styles from './GameCanvas.module.css';
-import Border from './Border';
+import { useEffect, useRef } from 'react'
+import styles from './GameCanvas.module.css'
+import Border from './Border'
 
 
 function GameCanvas() {
-    return (
-      <Border>
-        <canvas className={styles.game} width="800" height="600">
-          Tu navegador no soporta Canvas 😢
-        </canvas>
-      </Border>
-    );
-  }
-  
-  export default GameCanvas;
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (canvas.getContext) {
+      var ctx = canvas.getContext("2d")
+    }
+    const cellSize = 20;
+    const cols = canvas.width / cellSize;
+    const rows = canvas.height / cellSize;
+
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+    ctx.lineWidth = 1;
+
+    // Dibujar líneas verticales
+    for (let x = 0; x <= cols; x++) {
+      ctx.beginPath();
+      ctx.moveTo(x * cellSize, 0);
+      ctx.lineTo(x * cellSize, canvas.height);
+      ctx.stroke();
+    }
+
+    // Dibujar líneas horizontales
+    for (let y = 0; y <= rows; y++) {
+      ctx.beginPath();
+      ctx.moveTo(0, y * cellSize);
+      ctx.lineTo(canvas.width, y * cellSize);
+      ctx.stroke();
+    }
+
+  }, [])
+
+  return (
+    <Border>
+      <canvas
+        ref={canvasRef}
+        className={styles.game}
+        width={800}
+        height={600}
+      />
+    </Border>
+  );
+}
+
+export default GameCanvas
